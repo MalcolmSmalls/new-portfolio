d605be386d31a215b070b0c62b8749525745f21b
MalcolmSmalls <smallsbeats@gmail.com>
2023-08-17 12:44:18 -0700

    Add popdown navigation menu

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+const header = document.querySelector('header')
+const hamburger = document.querySelector('.nav-hamburger')
 const nav = document.querySelector('.nav')
-const body = document.querySelector('html')
+const navLink = document.querySelector('.nav-link')
 
 window.addEventListener('scroll', () => {
   if (window.scrollY !== 0) {
-    nav.classList.add('nav-white-bg')
+    header.classList.add('nav-white-bg')
   } else {
-    nav.classList.remove('nav-white-bg')
+    nav.classList.remove('open-nav')
+    header.classList.remove('nav-white-bg')
+  }
+})
+
+hamburger.addEventListener('click', () => {
+  header.classList.add('nav-white-bg')
+  nav.classList.toggle('open-nav')
+  navLink.classList.toggle('nav-link-colored')
+  if (window.scrollY === 0 && !nav.matches('.open-nav')) {
+    header.classList.remove('nav-white-bg')
   }
 })
