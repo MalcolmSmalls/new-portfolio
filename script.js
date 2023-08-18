fc0f27e797f50c4d239c082a6b69f30ec9b3a3d4
MalcolmSmalls <smallsbeats@gmail.com>
2023-08-18 13:14:15 -0700

    Navbar closes now on link click

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const header = document.querySelector('header')
 const hamburger = document.querySelector('.nav-hamburger')
 const nav = document.querySelector('.nav')
-const navLink = document.querySelector('.nav-link')
+const navLink = document.querySelectorAll('.nav-link')
 
 window.addEventListener('scroll', () => {
   if (window.scrollY !== 0) {
@@ -15,8 +15,16 @@ window.addEventListener('scroll', () => {
 hamburger.addEventListener('click', () => {
   header.classList.add('nav-white-bg')
   nav.classList.toggle('open-nav')
-  navLink.classList.toggle('nav-link-colored')
+
   if (window.scrollY === 0 && !nav.matches('.open-nav')) {
     header.classList.remove('nav-white-bg')
   }
+})
+
+const removeOpenNav = () => {
+  nav.classList.remove('open-nav')
+}
+
+navLink.forEach((element) => {
+  element.addEventListener('click', removeOpenNav)
 })
