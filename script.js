b8348f78142baaf21cb42cfc7ddfb0f82ed9d13b
MalcolmSmalls <smallsbeats@gmail.com>
2023-08-22 20:43:34 -0700

    Set up portfolio list item and effects

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const header = document.querySelector('header')
 const hamburger = document.querySelector('.nav-hamburger')
 const nav = document.querySelector('.nav')
 const navLink = document.querySelectorAll('.nav-link')
+const gridItem = document.querySelector('.grid-item')
+const shuffleImg = document.querySelector('.shuffle-grid-img')
+const shuffleContent = document.querySelector('.shuffle-grid-content')
 
 window.addEventListener('scroll', () => {
   if (window.scrollY !== 0) {
@@ -27,4 +30,22 @@ const removeOpenNav = () => {
 
 navLink.forEach((element) => {
   element.addEventListener('click', removeOpenNav)
+})
+
+gridItem.addEventListener('mouseover', () => {
+  shuffleImg.classList.add('scale')
+  shuffleContent.classList.add('reveal')
+})
+
+gridItem.addEventListener('mouseleave', () => {
+  shuffleImg.classList.remove('scale')
+  shuffleContent.classList.remove('reveal')
+})
+const Shuffle = window.Shuffle // Assumes you're using the UMD version of Shuffle (for example, from unpkg.com).
+const element = document.getElementById('portfolio-list')
+const sizer = element.querySelector('.js-shuffle-sizer')
+
+const shuffleInstance = new Shuffle(element, {
+  itemSelector: '.picture-item',
+  sizer: sizer, // could also be a selector: '.js-shuffle-sizer'
 })
